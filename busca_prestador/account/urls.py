790207e55c08b00452505a80cbe81cd14a12d0d4
Aleleonel <aleleonel@gmail.com>
2020-11-18 17:56:39 -0300

    registro usuário2

--- busca_prestador/account/urls.py
+++ busca_prestador/account/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views as v
 
+app_name = 'account'
+
 urlpatterns = [
-    path('', v.registrar, name='account'),
+    path('', v.registrar, name='registrar'),
 
 ]
