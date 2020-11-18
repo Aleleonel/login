950e966580eb44c917a851988f026445ef4962f8
Aleleonel <aleleonel@gmail.com>
2020-11-18 16:56:36 -0300

    formulario usuário autenticação

--- busca_prestador/account/views.py
+++ busca_prestador/account/views.py
@@ -1,7 +1,14 @@
 # Create your views here.
 from django.shortcuts import render, redirect
-from django.http import HttpResponseRedirect  # Funcao para redirecionar o usuario
-from django.contrib.auth.forms import UserCreationForm  # Formulario de criacao de usuarios
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+
+
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('/login/')
+    template_name = 'account/register.html'
 
 
 def registrar(request):
